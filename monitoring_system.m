fa7594e615654a0fca942d6bce77f193a1b513a3
HHai25 <163253919+jvinaito@users.noreply.github.com>
2026-04-14 14:21:07 +0700

    sửa lại chú thích thừa

--- monitoring_system.m
+++ monitoring_system.m
@@ -1,18 +1,18 @@
 function monitoring_system(otn_frame)
-    % otn_frame nên là một mảng các số nguyên từ 0-255 (các byte)
+  
     fprintf('--- Hệ thống giám sát BIP-8 Real-time ---\n');
     pause(0.5);
 
     % 1. Tính toán BIP-8 thực tế từ dữ liệu đầu vào
-    % Ta thực hiện XOR dồn tất cả các byte trong frame
+    % thực hiện XOR dồn tất cả các byte trong frame
     calculated_bip = 0;
     for i = 1:length(otn_frame)
         calculated_bip = bitxor(calculated_bip, otn_frame(i));
     end
 
     % 2. Giả lập byte BIP-8 nhận được từ Overhead (để so sánh)
-    % Bình thường nó phải khớp với calculated_bip. 
-    % Ta giả lập lỗi bằng cách thỉnh thoảng làm sai lệch nó đi.
+    % Bình thường  phải khớp với calculated_bip. 
+    % giả lập lỗi bằng cách thỉnh thoảng làm sai lệch nó đi.
     if rand > 0.8
         received_bip = bitxor(calculated_bip, randi([1, 255])); % Gây lỗi
     else
